83ad803c182f74b1cdbdd06fccba34fc87260a49
Stace <129897147+Stace05@users.noreply.github.com>
2024-11-29 18:24:46 +0200

    Create images.jsx

---
+++ src/components/image/images.jsx
@@ -0,0 +1,23 @@
+
+
+import bengal from './bengal.jpg';
+import british from './british.jpg';
+import chinese from './chinese.jpg';
+import syrian from './syrian.jpg';
+import german from './german.jpg';
+import terrier from './terrier.jpg';
+import persian from './persian.jpg';
+import maine_coon from './maine_coon.jpg';
+
+const images = {
+    'components/image/bengal.jpg': bengal,
+    'components/image/british.jpg': british,
+    'components/image/chinese.png': chinese,
+    'components/image/syrian.jpg': syrian,
+    'components/image/german.jpg': german,
+    'components/image/terrier.jpg': terrier,
+    'components/image/maine_coon.jpg': maine_coon,
+    'components/image/persian.jpg': persian
+};
+
+export default images;
